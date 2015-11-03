b11e047e80cd18d0d0babca286e107213ac65e69
TriHTM <trihtm123@gmail.com>
2015-11-03 11:06:54 +0700

    Complete verbose

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to use Firefox SDK Addons.
+
+## How to run addon on Firefox
+
+sudo jpm run -b /usr/lib/firefox/firefox
--- index.js
+++ index.js
@@ -1,9 +1,68 @@
 var self = require('sdk/self');
+var tabs = require("sdk/tabs");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
+var SimpleAddon = (function() {
+  var Addon = {};
 
-exports.dummy = dummy;
+  Addon.start = function () {
+    tabs.on('ready', function () {
+      if (Addon.User.isAccessFacebook()) {
+        if (User.shouldRemoveFacebook()) {
+          Addon.removeFacebook();
+        } else {
+          console.log('It\'s time to checking your Facebook. Enjoy!');
+        }
+      }
+    });
+  };
+
+  Addon.debug = function () {
+    // Some debug here
+  };
+
+  Addon.removeFacebook = function () {
+    console.log('I have removed Facebook content. Please focus your work.');
+
+    var contentScriptString = 'document.body.innerHTML = "<h1>You cannot access Facebook from 9AM to 5PM</h1>";'
+
+    tabs.activeTab.attach({
+      contentScript: contentScriptString
+    });
+  };
+
+  var User = {};
+
+  User.shouldRemoveFacebook = function () {
+    var date = new Date;
+
+    var currentHour = date.getHours();
+
+    var fromHour = 9;
+    var endHour = 17;
+
+    return currentHour >= fromHour && currentHour < endHour;
+  };
+
+  User.isAccessFacebook = function () {
+    var activeUrl = tabs.activeTab.url;
+
+    var regex = /facebook\.com/i;
+
+    if (regex.test(activeUrl)) {
+      console.log('You are accessing to Facebook.');
+
+      return true;
+    }
+
+    console.log('You are not accessing to Facebook.');
+
+    return false;
+  };
+
+  Addon.User = User;
+
+  return Addon;
+} () );
+
+SimpleAddon.start();
+SimpleAddon.debug();
